f06ccd4fb13eaf0c7f1e6a81430bd975b1fe5c01
Nikita_big_hater <nikita_tililicin@mail.ru>
2024-09-21 14:18:32 +0700

    First commit

---
+++ main.py
@@ -0,0 +1,25 @@
+
+
+#total_tasks = 12
+#print(total_tasks)
+#hours = 1.5
+#print(hours)
+#average_hours = hours / total_tasks
+#print(f'Среднее время на задание: {average_hours}')
+#print('Среднее время на задание: ' + str(average_hours))
+#print('Среднее время на задание:', str(average_hours))
+
+
+#Напишите в начале программы однострочный комментарий: "4th program".
+#Дана строка '123.456'.
+#Вывести на экран первую цифру после запятой - 4.
+
+string1=('123.456')
+#float_value=float(string1) # преабразовываем строку
+#float_value=float_value*10
+#result=int(float_value%10)
+#print(result)
+result = int((float(string1)*10)%10)
+print(result)
+result = int((float(string1)*10)%10)
+print(result)
